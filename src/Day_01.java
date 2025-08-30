ad1b9e0f54dce3b486b8c1914ca5600669aab322
Pooja <nikamp870@gmail.com>
2025-08-30 21:35:07 +0530

    git init

---
+++ src/Day_01.java
@@ -0,0 +1,16 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day_01 {
+    //    Print Array Elements → Take user input for an array and print it.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
