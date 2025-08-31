22238d8362072be7c9fc1d4a84fb0b52ea0c92df
Pooja <nikamp870@gmail.com>
2025-08-31 18:06:19 +0530

    added day 3 & 4

---
+++ src/Day_03.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day_03 {
+    //   Reverse an Array → Without using extra space.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        System.out.print("Enter " + size + " array elements: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length- 1 - i];
+            arr[arr.length - 1- i] = temp;
+
+        }
+        System.out.println("The reversed array is :" + Arrays.toString(arr));
+
+    }
+}
---
+++ src/Day_04.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Day_04 {
+    //   Second largest Element → Find the second largest element in the array.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.print("Enter " + size + " array elements: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (size<2){
+            System.out.println("Array must have at least two elements.");
+            return;
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secLarget = Integer.MIN_VALUE;
+
+        for (int num: arr) {
+            if (num > largest) {
+                secLarget = largest;
+                largest = num;
+            } else if (num < largest && num > secLarget) {
+                secLarget = num;
+            }
+        }
+        if (secLarget == Integer.MIN_VALUE) {
+            System.out.println("No second largest element (all elements are equal).");
+        } else {
+            System.out.println("Second largest element: " + secLarget);
+        }
+
+        sc.close();
+
+    }
+}
